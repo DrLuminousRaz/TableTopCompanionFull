7c49df019befcc053f68eb8dbc75ca980a8df3b6
DrLuminousRaz <drraz77@iinet.net.au>
2018-02-27 12:49:11 +0800

    Working SQLite Database, basic form functionality

---
+++ TTC/MainActivity.cs
@@ -0,0 +1,122 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System;
+using System.IO;
+using SQLite;
+using System.Collections.Generic;
+using System.Linq;
+using TTC.Resources.Model;
+
+namespace TTC
+{
+    
+    [Activity(Label = "TTC", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        public string dbPath = Path.Combine(System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal), "dbHistory.db3");
+
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            string folder = System.Environment.GetFolderPath(System.Environment.SpecialFolder.Personal);
+            int res;
+            var spinner = FindViewById<Spinner>(Resource.Id.spinner);
+            Button button = FindViewById<Button>(Resource.Id.button);
+            Button getbutton = FindViewById<Button>(Resource.Id.getButton);
+            var text = FindViewById<TextView>(Resource.Id.result);
+            string dateTime = DateTime.Now.ToString();
+            spinner.Prompt = "Choose A Dice";
+            var adapter = ArrayAdapter.CreateFromResource(this, Resource.Array.Counter, Android.Resource.Layout.SimpleSpinnerItem);
+            adapter.SetDropDownViewResource(Android.Resource.Layout.SimpleSpinnerDropDownItem);
+            spinner.Adapter = adapter;
+            
+            var db = new SQLiteConnection(dbPath);
+            db.CreateTable<History>();
+            
+
+
+            button.Click += delegate
+            {
+                
+                switch (spinner.SelectedItem.ToString())
+                {
+
+                    case "D4":
+
+                        res = Roll.Roll.D4();
+                        text.Text = "D4: " + res.ToString();
+                        History d4 = new History(dateTime, "D4", res);
+                        db.Insert(d4);
+                        break;
+                    case "D6":
+
+                        res = Roll.Roll.D6();
+                        text.Text = "D6: " + res.ToString();
+                        History d6 = new History(dateTime, "D6", res);
+                        db.Insert(d6);
+                        break;
+                    case "D8":
+
+                        res = Roll.Roll.D8();
+                        text.Text = "D8: " + res.ToString();
+                        History d8 = new History(dateTime, "D8", res);
+                        db.Insert(d8);
+                        break;
+                    case "D10":
+                        res = Roll.Roll.D10();
+                        text.Text = "D10: " + res.ToString();
+                        History d10 = new History(dateTime, "D10", res);
+                        db.Insert(d10);
+                        break;
+                    case "D10 Percentile":
+                        res = Roll.Roll.D10Pecentile();
+                        text.Text = "D10 Percentile: " + res.ToString();
+                        History d10p = new History(dateTime, "D10 Percentile", res);
+                        db.Insert(d10p);
+                        break;
+                    case "D12":
+                        res = Roll.Roll.D12();
+                        text.Text = "D12: " + res.ToString();
+                        History d12 = new History(dateTime, "D12", res);
+                        db.Insert(d12);
+                        break;
+                    case "D20":
+                        res = Roll.Roll.D20();
+                        text.Text = "D20: " + res.ToString();
+                        History d20 = new History(dateTime, "D20", res);
+                        db.Insert(d20);
+                        break;
+
+
+                }
+            };
+
+            getbutton.Click += delegate
+            {
+                TextView disp = FindViewById<TextView>(Resource.Id.textViewHistory);
+                disp.Text = "";
+                var conn = new SQLiteConnection(dbPath);
+                var table = conn.Table<History>();
+                
+                foreach(var item in table)
+                {
+                    History myHistory = new History(item.DateAndTime, item.Dice1Type, item.Result1);
+                    disp.Text += myHistory + "\n";
+                    
+                }
+                
+                
+            };
+        }
+
+        
+
+    }
+    }
+
+
---
+++ TTC/Resources/Model/History.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using SQLite;
+
+namespace TTC.Resources.Model
+{
+    
+        
+        class History : BaseModel
+    { 
+        public int Id { get; set; }
+        public string DateAndTime { get; set; }
+            public string Dice1Type { get; set; }
+            public int Result1 { get; set; }
+            /*   public string Dice2Type { get; set; }
+               public int Result2 { get; set; }
+               public string Dice3Type { get; set; }
+               public int Result3 { get; set; }
+               public string Dice4Type { get; set; }
+               public int Result4 { get; set; }
+               public string Dice5Type { get; set; }
+               public int Result5 { get; set; }
+               public string Dice6Type { get; set; }
+               public int Result6 { get; set; }
+               public string Dice7Type { get; set; }
+               public int Result7 { get; set; }
+               public string Dice8Type { get; set; }
+               public int Result8 { get; set; }
+               public string Dice9Type { get; set; }
+               public int Result9 { get; set; }
+               public string Dice10Type { get; set; }
+               public int Result10 { get; set; }
+               public string Dice11Type { get; set; }
+               public int Result11 { get; set; }
+               public string Dice12Type { get; set; }
+               public int Result12 { get; set; }
+               public string Dice13Type { get; set; }
+               public int Result13 { get; set; }
+               public string Dice14Type { get; set; }
+               public int Result14 { get; set; }
+               public string Dice15Type { get; set; }
+               public int Result15 { get; set; }
+               public string Dice16Type { get; set; }
+               public int Result16 { get; set; }
+               public string Dice17Type { get; set; }
+               public int Result17 { get; set; }
+               public string Dice18Type { get; set; }
+               public int Result18 { get; set; }
+               public string Dice19Type { get; set; }
+               public int Result19 { get; set; }
+               public string Dice20Type { get; set; }
+               public int Result20 { get; set; }
+               */
+
+            public History(string dateTime, string type, int res)
+            {
+            DateAndTime = dateTime;
+            Dice1Type = type;
+            Result1 = res;
+            
+            
+            }
+
+
+
+
+
+            public History()
+            {
+
+            }
+
+        public override string ToString()
+        {
+            return string.Format($"{DateAndTime}   Dice: {Dice1Type}   Result: {Result1}");
+        }
+
+    }
+    public abstract class BaseModel
+    {
+
+        [PrimaryKey, AutoIncrement]
+        public int PrimaryKey { get; set; }
+
+    }
+}
